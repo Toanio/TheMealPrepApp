69f15d053a6bc2cacdec0bbef98ae3981c0caebc
Toan <chan.misha95@mail.ru>
2022-10-08 08:58:55 +0300

    Initial Commit

---
+++ TheMealPrepApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMealPrepApp
+//
+//  Created by c.toan on 08.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
