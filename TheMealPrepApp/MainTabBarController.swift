83a07a627d74829933ae4bc9b1fc420b2c5e7dad
Toan <chan.misha95@mail.ru>
2022-10-08 09:14:05 +0300

    - First commit

---
+++ TheMealPrepApp/MainTabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  MainTabBarController.swift
+//  TheMealPrepApp
+//
+//  Created by c.toan on 08.10.2022.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
